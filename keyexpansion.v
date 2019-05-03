1bc02f123b530dd31d8c9bbcd6da7ac8e54adffb
muhammad20 <muhammadahmad201196@hotmail.com>
2019-05-04 01:42:22 +0200

    key expansion is synthesizable

--- keyexpansion.v
+++ keyexpansion.v
@@ -39,122 +39,80 @@ mux4x1 m1(key[95:64], key[159:128], key[223:192], zero_reg, mux4x1_sel, w_array_
 mux4x1 m0(key[127:96], key[191:160], key[255:224], zero_reg, mux4x1_sel, w_array_fixed[0]);
 
 
-reg [31:0] temp_op;
+wire [31:0] temp_op;
 reg [5:0] counter;
-
 wire [31:0] temp_fixed, temp_rot_fixed, temp_sub_fixed, xor_fixed;
-wire en_wire;
-assign en_wire = 1'b0;
-reg en;
-reg [31:0] Rconi;
-reg [31:0] w_temp;
-reg done, err;
-wire [31:0] w_normal, w_8, w_mod;
+wire [31:0] w_temp;
+wire [31:0] w_normal, w_8, w_mod, w_out;
+wire [2:0] count_mod_Nk;
+wire iter_result_least_selector, iter_result_most_selector;
+assign count_mod_Nk = counter % Nk;
+assign iter_result_least_selector = ~(count_mod_Nk[0] | count_mod_Nk[1] | count_mod_Nk[2]);
+assign iter_result_most_selector = Nk[3] & count_mod_Nk[2] & ~count_mod_Nk[1] & ~count_mod_Nk[0];
+wire [1:0] iter_result_selector;
+assign iter_result_selector = {iter_result_most_selector, iter_result_least_selector};
 
+wire [31:0] Rconii;
+assign Rconii = Rcon[counter/Nk - 1];
 
+wire en;
+assign en = (counter >= Nk) ? 1'b1 : 1'b0;
+
+assign done = (counter >= 4 * (Nk + 6 + 1)) ? 1'b1 : 1'b0;
+
+assign err = (~(Nk[3] ^ Nk[2]) | Nk[2] | (Nk[3] & Nk[1])) ? 1'b1 : 1'b0;
+
+// ------------ if Counter mod Nk == 0
 RotWord rot(temp_op, temp_rot_fixed);
 SubWord sub(temp_rot_fixed, temp_sub_fixed);
-xor32 myXor(temp_sub_fixed, Rconi, xor_fixed);
+xor32 myXor(temp_sub_fixed, Rconii, xor_fixed);
 xor32 myXor2(xor_fixed, w_temp, w_mod);
+
+// ----------- normal oerpation
 xor32 myXor3(w_temp, temp_op, w_normal);
+
+// ----------- if Nk = 8 and counter mod Nk == 4
 SubWord sub2(temp_op, temp_fixed);
 xor32 myXor4(temp_fixed, w_temp, w_8);
 
-always @(Nk)
-    begin
-        if (Nk != 4 && Nk != 6 && Nk != 8)
-            err = 1'b1;
-        else
-            err = 1'b0;
-    end
+// ---------- choose iteration result and store it in w_array
+mux4x1 iter_result(w_normal, w_mod, w_8, w_8, iter_result_selector, w_out);
 
+// -----w[i]
+assign temp_op = w_array[counter - 1];
 
-always @(posedge clk, rst, err)
+// -----w[i - Nk]
+assign w_temp = w_array[counter - Nk];
+
+always @(clk, rst, err, done)
     begin
         if(rst)
-	begin
-        counter = 5'b0;
-	    en = en_wire;
+        	counter = 6'b0;
+		else if(!err && !done) begin
+			counter = counter + 6'h1;		
+if(en && !done)
+			w_array[counter - 1] = w_out;	
+	if (counter == 6'h1 && !err) begin
+		w_array[0] = w_array_fixed[0];
+		w_array[1] = w_array_fixed[1];
+		w_array[2] = w_array_fixed[2];
+		w_array[3] = w_array_fixed[3];
+		w_array[4] = w_array_fixed[4];
+		w_array[5] = w_array_fixed[5];
+		w_array[6] = w_array_fixed[6];
+		w_array[7] = w_array_fixed[7];
 	end
-	else if(!err)
-	begin
-        if(counter == Nk)
-	    begin
-        if (Nk < 8)
-        begin
-            w_array[59] <= zero_reg;
-            w_array[58] <= zero_reg;
-            w_array[57] <= zero_reg;
-            w_array[56] <= zero_reg;
-            w_array[55] <= zero_reg;
-            w_array[54] <= zero_reg;
-            w_array[53] <= zero_reg;
-            w_array[52] <= zero_reg;
-        end
-		if(Nk == 4)
-		begin
-	    w_array[51] <= zero_reg;
-            w_array[50] <= zero_reg;
-            w_array[49] <= zero_reg;
-            w_array[48] <= zero_reg;
-            w_array[47] <= zero_reg;
-            w_array[46] <= zero_reg;
-            w_array[45] <= zero_reg;
-            w_array[44] <= zero_reg;        
-		end
-	        done = 1'b0;
-            	en = 1'b1;
-	    end
-        else if(counter == 1)
-        begin
-            w_array[0] <= w_array_fixed[0];
-            w_array[1] <= w_array_fixed[1];
-            w_array[2] <= w_array_fixed[2];
-            w_array[3] <= w_array_fixed[3];
-            w_array[4] <= w_array_fixed[4];
-            w_array[5] <= w_array_fixed[5];
-            w_array[6] <= w_array_fixed[6];
-            w_array[7] <= w_array_fixed[7];
-            counter <= counter + 5'b1;
-	end
-	else if (!en)
-	    counter <= counter + 5'b1;
-    end
-    end
-    
-
-always @(posedge clk, en, done, err)
-    begin
-        if(!err)
-            if(en && !done && !err)
-                begin
-                    temp_op = w_array[counter - 1];
-                    if(counter % Nk == 0)
-                        Rconi = Rcon[counter / Nk - 1];
-                counter = counter + 1'b1;
-                w_temp = w_array[counter - Nk - 1];
-        end
-    end
-
-always @(negedge clk, en)
-	begin
-	if(en && !done && !err)
-begin
-	if((counter - 1) % Nk == 0)
-		w_array[counter - 1] = w_mod;
-	else if (Nk > 6 && (counter - 1) % Nk == 4)
-		w_array[counter - 1] = w_8;
-	else
-            w_array[counter - 1] = w_normal;
-            if (counter == 4 * (Nk + 6 + 1))
-                done = 1'b1;
 end
-	end
+end
+
 
 genvar k;
-for (k = 0; k < 60; k = k + 1)
+generate
+for (k = 0; k < 60; k = k + 1) begin : getOutput
     assign w[32*k + 31: 32*k] = w_array[k];
-
+end
+endgenerate
+	
 endmodule
 
 
